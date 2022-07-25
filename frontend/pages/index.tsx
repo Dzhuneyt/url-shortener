5653b2002980dce1e096a825110aaa2440eca581
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2022-07-25 22:49:18 +0300

    feat: OpenGraph tags

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,6 +6,7 @@ import {ShortenedResultWithCopyToClipboard} from "../components/ShortenedResultW
 import {getOrigin} from "../util/getOrigin";
 import {Center} from "@mantine/core";
 import Footer from "../components/Footer";
+import Head from 'next/head';
 
 const Home: NextPage = () => {
     const [shortenedUrl, setShortenedUrl] = useState<string>('')
@@ -13,7 +14,15 @@ const Home: NextPage = () => {
 
     return (
         <>
+            <Head>
+                <title>URL shortener - gotogo.click</title>
+                <meta property="og:type" content="website"/>
+                <meta property="og:title" content="URL Shortener"/>
+                <meta property="og:description" content="undefined"/>
+                <meta property="og:image" content="https://gotogo.click/photo-1517404215738-15263e9f9178.jpeg"/>
+            </Head>
             <div>
+                <img src={'/photo-1517404215738-15263e9f9178.jpeg'}/>
                 <ShortenField
                     onShortened={(id) => setShortenedUrl(`${getOrigin()}${router.asPath}go/${id}`)}/>
                 <ShortenedResultWithCopyToClipboard url={shortenedUrl}/>
